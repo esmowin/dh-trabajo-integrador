f1eef276a5cd8f9c6ac595fc9642ac1d882a83e8
Agustin Vilanova <luguergus.lv@gmail.com>
2018-09-28 00:59:58 -0300

    Hice el maquetado general,e individualice cada archivo .css

---
+++ home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Smart Pet´s</title>
+  </head>
+  <body>
+
+    <header>
+      <nav class="usuario">
+        <a href="registro.php">Registrar</a>
+        <a href="login.php">Login</a>
+      </nav>
+
+      <nav >
+        <ul class="barra_nav">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Mascotas</a></li>
+          <li><a href="#">Ofertas</a></li>
+          <li><a href="#">Contacto</a></li>
+          <li><a href="#">ONG</a></li>
+        </ul>
+      </nav>
+
+    </header>
+
+
+    <section class="title">
+      <h1>Bienvenido a Smart Pet´s</h1>
+    </section>
+
+    <nav>
+      <ul class="sideBar">
+        <li><a href="#">Perros</a></li>
+        <li><a href="#">Gatos</a></li>
+        <li><a href="#">Aves</a></li>
+        <li><a href="#">Show room</a></li>
+        <li><a href="#">Smart food</a></li>
+      </ul>
+    </nav>
+
+
+      <footer >
+        <h2>Development by:......</h3>
+
+      </footer>
+
+  </body>
+
+</html>
