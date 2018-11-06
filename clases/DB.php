adb1cdf9df895704afc49d629b5050a183f0050a
fedeBunge <federicobunge@gmail.com>
2018-11-06 15:57:57 -0300

    Se crearon los archivos para las clases y la base de datos smartpet. En la clase DB se creo el constructor y dos metodos, traerTodo y traerPorEmail

---
+++ clases/DB.php
@@ -0,0 +1,91 @@
+<?php
+
+class BD
+{
+
+  private $dsn = "mysql:host=localhost;dbname=smartpet;";
+  private $usuario = "root";
+  private $pass = "root";
+  private $opt=[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION];
+  private $conn;
+
+  function __construct()
+  {
+    try {
+      $this->conn=new PDO($this->dsn, $this->usuario, $this->pass,$this->opt);
+    } catch (Exception $e) {
+      echo "Error en la conexion a la base de datos.";
+    }
+  }
+
+    public function guardarUsuario(Usuario $usuario){
+
+      $stmt = $this->conn->prepare("INSERT INTO usuarios(fullname, nickname, country, email, password, avatar, activo) VALUES (:fullname, :nickname, :country, :email, :password, :avatar, 1)");
+
+      $stmt->bindValue(":fullname",$usuario->getNombre());
+      $stmt->bindValue(":nickname",$usuario->getNickname());
+      $stmt->bindValue(":country",$usuario->getCountry());
+      $stmt->bindValue(":email",$usuario->getEmail());
+      $stmt->bindValue(":password",$usuario->getPassword());
+      $stmt->bindValue(":avatar",$usuario->getAvatar());
+
+      $stmt->execute();
+      $idUsuario = $this->conn->lastInsertId();
+      $usuario->setId($idUsuario);
+
+      return $usuario;
+    }
+
+    public function traerTodo(){
+      $stmt = $this->conn->prepare("SELECT * FROM usuarios");
+
+      $stmt->execute();
+
+      $resultados = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+      foreach ($resultados as $cadaUsuario) {
+        $usuarios[] = new Usuario(
+          $cadaUsuario->idUsuario,
+          $cadaUsuario->fullname,
+          $cadaUsuario->nickname,
+          $cadaUsuario->country,
+          $cadaUsuario->email,
+          $cadaUsuario->password,
+          $cadaUsuario->avatar)
+          $cadaUsuario->activo);
+      }
+      return $usuarios;
+    }
+
+    public function traerPorEmail($email) {
+      $stmt = $this->conn->prepare("SELECT * FROM usuarios WHERE email = :email" );
+
+      $stmt->bindValue(":email",$email);
+
+      $stmt-> execute();
+
+      $resultados = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      if ($resultados){
+        return new Usuario(
+          $resultados['idUsuario'],
+          $resultados['fullname'],
+          $resultados['nickname'],
+          $resultados['country'],
+          $resultados['email'],
+          $resultados['password'],
+          $resultados['avatar'],
+          $resultados['activo']);
+
+      }else{
+        return null;
+      }
+    }
+  }
+
+
+
+
+
+
+ ?>
