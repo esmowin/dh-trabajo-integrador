3526f31ff7beac10d6e7cd7711c06f233ab7544e
gabb90 <gabrielhocsman@gmail.com>
2018-11-07 18:16:02 -0300

    class auth

---
+++ clases/Auth.php
@@ -0,0 +1,23 @@
+<?php
+
+  class auth
+  {
+
+    function __construct($username, $password)
+    {
+      
+    }
+
+    function login($username, $password){
+
+
+      return $user;
+
+    }
+
+    function recuerdame(){
+      $_COOKIE
+      $_SESSION
+
+    }
+  }
