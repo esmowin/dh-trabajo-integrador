411e698a156be6cfa030ccbe7c9ef528a5464c81
AgustinVilanova <luguergus.lv@gmail.com>
2018-11-08 14:42:55 -0300

    Implementacion de clase Validator -

---
+++ clases/Validator.php
@@ -0,0 +1,121 @@
+<?php
+
+class Validator{
+
+    private $errores = [];
+    private $fullname = '';
+    private $nickname = '';
+    private $country = '';
+    private $email = '';
+    private $password1 = '';
+    private $password2 = '';
+    private $paises = ['Argentina', 'Bolivia', 'Brasil', 'Chile', 'Colombia', 'Ecuador', 'México', 'Paraguay', 'Perú', 'Uruguay', 'Venezuela'];
+
+    public function validarPost(Array $post){
+
+      // "Limpio" los datos recibidos por POST
+
+      $this->fullname = trim($post['fullname']);
+      $this->nickname = trim($post['nickname']);
+      $this->country = trim($post['country']);
+      $this->email = trim($post['email']);
+      $this->password1 = trim($post['password1']);
+      $this->password2 = trim($post['password2']);
+
+      // Valido el "Nombre completo"
+
+      // Hago un tratamiento para NO permitir símbolos como *, $, &, /, etc., pero SÍ permitir las letras utilizadas tanto en el idioma español como en el portugués -esto incluye las letras "especiales" de estos dos idiomas, es decir, la Ñ y la Ç-, así como los espacios)
+
+      if (empty($this->fullname)) {
+        $this->errores ['fullname'] = 'Debes completar este campo';
+      } else if ((strlen($this->fullname) < 4) || (strlen($this->fullname) > 60)) {
+        $this->errores ['fullname'] = 'El nombre completo debe tener un mínimo de 4 y un máximo de 60 caracteres';
+      } else if (!ctype_alpha(str_replace([' ', 'á', 'ã', 'â', 'à', 'é', 'ê', 'í', 'ó', 'õ', 'ô', 'ú', 'ñ', 'ç'], '', $this->fullname))) {
+        $this->errores ['fullname'] = 'Se ingresaron caracteres inválidos. Este campo acepta solo letras y espacios';
+      }
+
+      // Valido el "Nombre de usuario"
+
+      // Hago un tratamiento para NO permitir espacios ni símbolos como *, $, &, /, etc., pero SÍ permitir las letras utilizadas tanto en el idioma español como en el portugués -esto incluye las letras "especiales" de estos dos idiomas, es decir, la Ñ y la Ç-)
+
+      if (empty($this->nickname)) {
+        $this->errores ['nickname'] = 'Debes completar este campo';
+      } else if ((strlen($this->nickname) < 3) || (strlen($this->nickname) > 30)) {
+        $this->errores ['nickname'] = 'El nombre de usuario debe tener un mínimo de 3 y un máximo de 30 caracteres';
+      } else if (!ctype_alnum(str_replace(['á', 'ã', 'â', 'à', 'é', 'ê', 'í', 'ó', 'õ', 'ô', 'ú', 'ñ', 'ç'], '', $this->nickname))) {
+        $this->errores ['nickname'] = 'Se ingresaron caracteres inválidos. Este campo acepta solo letras y números (sin espacios)';
+      }
+
+      // Valido el "País de nacimiento"
+
+
+
+      if (empty($this->country)) {
+        $this->errores ['country'] = 'Debes elegir una opción';
+      } else if (!in_array($this->country, $this->paises)) {
+        $this->errores ['country'] = 'La opción ingresada no es válida';
+      }
+
+      // Valido el "Correo electrónico"
+
+      if (empty($this->email)) {
+        $this->errores ['email'] = 'Debes completar este campo';
+      } else if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+        $this->errores ['email'] = 'El formato del correo es inválido';
+      }
+
+      // Valido la "Contraseña"
+
+      if (empty($this->password1)) {
+        $this->errores ['password1'] = 'Debes completar este campo';
+      } else if ((strlen($this->password1) < 8) || (strlen($this->password1) > 20)) {
+        $errores ['password1'] = 'La contraseña debe tener un mínimo de 8 y un máximo de 20 caracteres';
+      }
+
+      // Valido el "Repetir contraseña"
+
+      if (empty($this->password2)) {
+        $this->errores ['password2'] = 'Debes completar este campo';
+      } else if ((strlen($this->password2) < 8) || (strlen($this->password2) > 20)) {
+        $this->errores ['password2'] = 'La contraseña debe tener un mínimo de 8 y un máximo de 20 caracteres';
+      } else if ($this->password2 !== $this->password1) {
+        $this->errores ['password2'] = 'Las contraseñas ingresadas no coinciden';
+      }
+
+      // Validando la "Imagen de perfil":
+
+      if (empty($this->errores)) {
+
+        if ($_FILES["avatar"]["error"] == 4) {
+
+          $this->errores ['imagen'] = "La imagen de perfil es obligatoria";
+
+        } else if ($_FILES["avatar"]["error"] == UPLOAD_ERR_OK) {
+
+          $archivo = uniqid();
+          $ext = pathinfo($_FILES["avatar"]["name"], PATHINFO_EXTENSION);
+          $desde = $_FILES["avatar"]["tmp_name"];
+
+          if ($ext != "png" && $ext != "jpg" && $ext != "jpeg") {
+            $this->errores ['imagen'] = "El formato del archivo es inválido";
+          } else if ($_FILES["avatar"]["size"] > 2097152) {
+            $this->errores ['imagen'] = "El archivo supera el tamaño máximo permitido";
+          } else {
+            $hasta = "avatars/".$archivo.".".$ext;
+            move_uploaded_file($desde, $hasta);
+
+          };
+
+        } else {
+          $this->errores['imagen'] = "Ocurrió un error al cargar la imagen de perfil";
+        }
+
+      }
+
+      return $this->errores;
+
+    }
+
+
+
+  }
--- registro.php
+++ registro.php
@@ -14,7 +14,6 @@
     // Defino las variables que voy a usar en las validaciones
 
     $errores = [];
-
     $fullname = '';
     $nickname = '';
     $country = '';
@@ -26,7 +25,14 @@
 
     if ($_POST) {
 
-      // "Limpio" los datos recibidos por POST
+      require_once 'clases/Validator.php';
+
+      $validador = new Validator();
+
+      $errores=$validador->validarPost($_POST);
+
+      // // "Limpio" los datos recibidos por POST
+      //
 
       $fullname = trim($_POST['fullname']);
       $nickname = trim($_POST['nickname']);
@@ -35,98 +41,10 @@
       $password1 = trim($_POST['password1']);
       $password2 = trim($_POST['password2']);
 
-      // Valido el "Nombre completo"
-
-      // Hago un tratamiento para NO permitir símbolos como *, $, &, /, etc., pero SÍ permitir las letras utilizadas tanto en el idioma español como en el portugués -esto incluye las letras "especiales" de estos dos idiomas, es decir, la Ñ y la Ç-, así como los espacios)
-
-      if (empty($fullname)) {
-        $errores['fullname'] = 'Debes completar este campo';
-      } else if (strlen($fullname) < 4 || strlen($fullname) > 60) {
-        $errores['fullname'] = 'El nombre completo debe tener un mínimo de 4 y un máximo de 60 caracteres';
-      } else if (!ctype_alpha(str_replace([' ', 'á', 'ã', 'â', 'à', 'é', 'ê', 'í', 'ó', 'õ', 'ô', 'ú', 'ñ', 'ç'], '', $fullname))) {
-        $errores['fullname'] = 'Se ingresaron caracteres inválidos. Este campo acepta solo letras y espacios';
+       if (empty($errores)){
+          // Una vez validado todo y subido exitosamente el archivo, vuelvo al Home (más adelante hay que hacer una pantalla que le dé la bienvenida al nuevo usuario (usando GET)
+        header('Location:home.php');
       }
-
-      // Valido el "Nombre de usuario"
-
-      // Hago un tratamiento para NO permitir espacios ni símbolos como *, $, &, /, etc., pero SÍ permitir las letras utilizadas tanto en el idioma español como en el portugués -esto incluye las letras "especiales" de estos dos idiomas, es decir, la Ñ y la Ç-)
-
-      if (empty($nickname)) {
-        $errores['nickname'] = 'Debes completar este campo';
-      } else if (strlen($nickname) < 3 || strlen($nickname) > 30) {
-        $errores['nickname'] = 'El nombre de usuario debe tener un mínimo de 3 y un máximo de 30 caracteres';
-      } else if (!ctype_alnum(str_replace(['á', 'ã', 'â', 'à', 'é', 'ê', 'í', 'ó', 'õ', 'ô', 'ú', 'ñ', 'ç'], '', $nickname))) {
-        $errores['nickname'] = 'Se ingresaron caracteres inválidos. Este campo acepta solo letras y números (sin espacios)';
-      }
-
-      // Valido el "País de nacimiento"
-
-      $paises = ['Argentina', 'Bolivia', 'Brasil', 'Chile', 'Colombia', 'Ecuador', 'México', 'Paraguay', 'Perú', 'Uruguay', 'Venezuela'];
-
-      if (empty($country)) {
-        $errores['country'] = 'Debes elegir una opción';
-      } else if (!in_array($country, $paises)) {
-        $errores['country'] = 'La opción ingresada no es válida';
-      }
-
-      // Valido el "Correo electrónico"
-
-      if (empty($email)) {
-        $errores['email'] = 'Debes completar este campo';
-      } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errores['email'] = 'El formato del correo es inválido';
-      }
-
-      // Valido la "Contraseña"
-
-      if (empty($password1)) {
-        $errores['password1'] = 'Debes completar este campo';
-      } else if (strlen($password1) < 8 || strlen($password1) > 20) {
-        $errores['password1'] = 'La contraseña debe tener un mínimo de 8 y un máximo de 20 caracteres';
-      }
-
-      // Valido el "Repetir contraseña"
-
-      if (empty($password2)) {
-        $errores['password2'] = 'Debes completar este campo';
-      } else if (strlen($password2) < 8 || strlen($password2) > 20) {
-        $errores['password2'] = 'La contraseña debe tener un mínimo de 8 y un máximo de 20 caracteres';
-      } else if ($password2 !== $password1) {
-        $errores['password2'] = 'Las contraseñas ingresadas no coinciden';
-      }
-
-      // Validando la "Imagen de perfil":
-
-      if (empty($errores)) {
-
-        if ($_FILES["avatar"]["error"] == 4) {
-
-          $errores['imagen'] = "La imagen de perfil es obligatoria";
-
-        } else if ($_FILES["avatar"]["error"] == UPLOAD_ERR_OK) {
-
-          $archivo = uniqid();
-          $ext = pathinfo($_FILES["avatar"]["name"], PATHINFO_EXTENSION);
-          $desde = $_FILES["avatar"]["tmp_name"];
-
-          if ($ext != "png" && $ext != "jpg" && $ext != "jpeg") {
-            $errores['imagen'] = "El formato del archivo es inválido";
-          } else if ($_FILES["avatar"]["size"] > 2097152) {
-            $errores['imagen'] = "El archivo supera el tamaño máximo permitido";
-          } else {
-            $hasta = "avatars/".$archivo.".".$ext;
-            move_uploaded_file($desde, $hasta);
-
-            // Una vez validado todo y subido exitosamente el archivo, vuelvo al Home (más adelante hay que hacer una pantalla que le dé la bienvenida al nuevo usuario (usando GET)
-
-            header('Location:home.php');
-
-          };
-
-        } else {
-          $errores['imagen'] = "Ocurrió un error al cargar la imagen de perfil";
-        };
-      };
 
     }
 
@@ -152,6 +70,7 @@
           <div class="registro-nombre-y-campo">
             <label for="fullname" class="registro-nombre">Nombre completo:</label>
             <div class="registro-campo">
+
               <input <?php if (isset($errores['fullname'])) {echo 'style="border: solid 2px red"';} ?> type="text" name="fullname" value="<?php echo $fullname ?>">
               <?php
                 if (isset($errores['fullname'])) {
@@ -213,7 +132,7 @@
           <div class="registro-nombre-y-campo">
             <label for="password1" class="registro-nombre">Contraseña:</label>
             <div class="registro-campo">
-              <input <?php if (isset($errores['password1']) || isset($errores['password2'])) {echo 'style="border: solid 2px red"';} ?> type="password" name="password1" value="">
+              <input <?php if (isset($errores['password1'])|| (isset($errores['password2']))) {echo 'style="border: solid 2px red"';} ?> type="password" name="password1" value="">
               <?php
                 if (isset($errores['password1'])) {
                   echo '<br><span class="registro-error">'.$errores['password1'].'</span>';
@@ -253,6 +172,7 @@
         <button class="registro-button" type="submit" name="button">Enviar</button>
 
       </form>
+
 
     </div>
 
