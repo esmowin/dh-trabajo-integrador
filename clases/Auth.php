0478595f57d5eb6f1a2a2fbf51e47d2660e35a7d
gabb90 <gabrielhocsman@gmail.com>
2018-11-09 20:53:17 -0300

    modificacion auth.php

--- clases/Auth.php
+++ clases/Auth.php
@@ -3,22 +3,41 @@ require_once 'DB.php';
 
   class Auth
   {
-
-
-    function login($username, $password){
-
-      if () {
-        // el usuario existe
-        return true;
+    public function __construct()// -crear constructor con session_start() y chequear que si esta la cookie de logueado, usarla como session
+    {
+      if (isset($_COOKIE["email"])) {
+        session_start();
+        $_SESSION["email"] = $_COOKIE["email"];
       }
 
-      return false;//el usuario no existe o la contrasena es incorrecta
+    }
+    public function loguear($email)// variable session con el email del parametro
+    {
+      session_start();
+      $_SESSION["email"] = $email;
 
     }
-
-    function recuerdame(){
-      $_COOKIE
-      $_SESSION
+    public function estaLogueado()// fijarse si esta seteado  la variable de sesion
+    {
+      return isset($_SESSION["email"]);
 
     }
+    public function usuarioLogueado($db=new BD)// que recibe una db. Preguntar si estaLogueado y devolver el objeto usuario con el traerPorEmail del db usando el email de sesion, sino devolver NULL
+    {
+      if ($this->estaLogueado()) {
+        return $db->traerPorEmail($_SESSION["email"]);
+      }else {
+        return NULL;
+      }
+    }
+    public function logout()// para session y cookies
+    {
+      setcookie("email", null, time()-1);
+      session_destroy();
+    }
+    public function recordarme($email)// que recibe un email y setea la cookie
+    {
+      setcookie("email", $email);
+    }
+
   }
