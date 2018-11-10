a9be50648ce5703b9a6520a5d5356a646aa2352e
fedeBunge <federicobunge@gmail.com>
2018-11-10 16:57:20 -0300

    Se agregaron require_once a clases y al registro

--- clases/DB.php
+++ clases/DB.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("Usuario.php");
+require_once 'clases/Usuario.php';
 
 class DB
 {
--- clases/Validator.php
+++ clases/Validator.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("DB.php");
+require_once 'clases/DB.php';
 
 class Validator{
 
